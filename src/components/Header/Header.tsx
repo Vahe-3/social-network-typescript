e1c42c686652042f73f40d198c8dc5662579272e
Vahe-3 <vahe.avetisyan333@gmail.com>
2022-06-09 17:48:18 +0400

    ready-made links in the header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,46 @@
 import React from 'react';
 import styles from "./Header.module.scss";
 import logo from "../../icons/logo.png";
-import {NavLink} from "react-router-dom";
-
+import {NavLink, useParams} from "react-router-dom";
+import usersIcon from "./../../icons/users.svg";
+import newsIcon from "./../../icons/news.svg";
+import messagesIcon from "./../../icons/messages.svg";
+import homeIcon from "./../../icons/home.svg"
 
 const Header : React.FC  = () => {
+
+
+
     return (
         <div className={styles.header}>
 
-                <div>
-                   <NavLink to={"/"} ><img src={logo}/></NavLink>
-                    <NavLink to={"/users"} > Users </NavLink>
+                <div  className={styles.header_logo} >
+                   <NavLink to={"/"}  ><img src={logo}/></NavLink>
                 </div>
-                <div>
+
+                <div  className={styles.header_title}>
                     <h2>Life Story</h2>
                 </div>
+
+                <div  className={styles.header_links}>
+                    <div   className={styles.header_links_link}>
+                        <NavLink to={"/profile"} >  <img src={homeIcon} /> </NavLink>
+                    </div>
+
+                    <div   className={styles.header_links_link}>
+                        <NavLink to={"/messages"} >  <img src={messagesIcon} /> </NavLink>
+                    </div>
+
+                    <div   className={styles.header_links_link}>
+                        <NavLink to={"/users"} > <img src={usersIcon} /> </NavLink>
+                    </div>
+
+                    <div   className={styles.header_links_link}>
+                        <NavLink to={"/news"} >  <img src={newsIcon} /> </NavLink>
+                    </div>
+
+                </div>
+
 
 
 
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import styles from "./Main.module.scss"
 import Profile from "./Profile/Profile";
-import {useAppDispatch} from "../../hooks/hooks";
 import {Route, Routes} from "react-router-dom";
 import Users from "./Users/Users";
+import Messages from "./Messages/Messages";
+import News from "./News/News";
 
 
 const Main: React.FC = () => {
@@ -14,7 +15,9 @@ const Main: React.FC = () => {
                 <Route path="/" element={<Profile />} />
                 <Route path="/profile/:id" element={<Profile />} />
                 <Route path="/profile" element={<Profile />} />
+                <Route path="/messages" element={<Messages />} />
                 <Route path="/users" element={<Users />} />
+                <Route path="/news" element={<News />} />
             </Routes>
         </div>
     );
